265dcd9b7b3e54281df2fa034a3b702b964d47b4
escapin <me@escapin.net>
2015-02-19 15:41:12 +0100

    first draft

--- Makefile
+++ Makefile
@@ -6,12 +6,10 @@ BCPROV_v=1.46
 JUNIT_v=4.12
 HARMCRESTCORE_v=1.3
 
-default:
-	@echo Specify the goal: devenv OR  devclean OR cleanElection
 
-devenv: java_download java_compile npm_install configs mix_configs copy_files download
 
-test: test_download test_configs test_run
+default: java_download java_compile 
+
 
 java_download:
 	-mkdir -p lib
@@ -22,103 +20,7 @@ java_compile:
 	javac -sourcepath src \
           -classpath "lib/*" \
           -d bin \
-          src/selectvoting/system/wrappers/*.java    
+          src/selectvoting/system/core/*.java    
 
-npm_install:
-	cd BulletinBoard; npm install
-	cd CollectingServer; npm install
-	cd MixServer; npm install
-	cd VotingBooth; npm install
-	cd node_modules/cryptofunc; npm install
-	
-configs:
-	-mkdir -p tmp
-	cp templates/*.pem tmp/
-	cp templates/ElectionManifest.json tmp/
-	cp templates/config_bb.json BulletinBoard/config.json
-	cp templates/config_cs.json CollectingServer/config.json
-	cp templates/config_mix.json MixServer/config.json
-	node tools/manifest2js.js templates/ElectionManifest.json > VotingBooth/webapp/ElectionManifest.js
-	
-mix_configs:
-	python configMixServers.py
-
-copy_files:
-	cp node_modules/voterClient.js VotingBooth/webapp/js/voterClient.js
-	cp node_modules/cryptofunc/index.js VotingBooth/webapp/js/cryptofunc.js
-
-download:
-	-rm VotingBooth/webapp/js/jquery-1.11.1.min.js
-	cd VotingBooth/webapp/js; wget http://code.jquery.com/jquery-1.11.1.min.js
-	-rm VotingBooth/webapp/pure/pure-min.css
-	cd VotingBooth/webapp/pure; wget http://yui.yahooapis.com/pure/0.5.0/pure-min.css
-	-rm VotingBooth/webapp/pure/grids-responsive-old-ie-min.css
-	cd VotingBooth/webapp/pure; wget http://yui.yahooapis.com/pure/0.5.0/grids-responsive-old-ie-min.css
-	-rm VotingBooth/webapp/pure/grids-responsive-min.css
-	cd VotingBooth/webapp/pure; wget http://yui.yahooapis.com/pure/0.5.0/grids-responsive-min.css 
-	-rm BulletinBoard/public/js/jquery-1.11.1.min.js
-	cd BulletinBoard/public/js; wget http://code.jquery.com/jquery-1.11.1.min.js
-	-rm BulletinBoard/public/pure/pure-min.css
-	cd BulletinBoard/public/pure; wget http://yui.yahooapis.com/pure/0.5.0/pure-min.css
-	-rm BulletinBoard/public/pure/grids-responsive-old-ie-min.css
-	cd BulletinBoard/public/pure; wget http://yui.yahooapis.com/pure/0.5.0/grids-responsive-old-ie-min.css
-	-rm BulletinBoard/public/pure/grids-responsive-min.css
-	cd BulletinBoard/public/pure; wget http://yui.yahooapis.com/pure/0.5.0/grids-responsive-min.css 
-
-test_download:
-	-mkdir -p lib
-	wget -P lib -nc http://central.maven.org/maven2/junit/junit/${JUNIT_v}/junit-${JUNIT_v}.jar
-	wget -P lib -nc http://central.maven.org/maven2/org/bouncycastle/bcprov-${BCPROV_t}/${BCPROV_v}/bcprov-${BCPROV_t}-${BCPROV_v}.jar
-	wget -P lib -nc https://hamcrest.googlecode.com/files/hamcrest-core-${HARMCRESTCORE_v}.jar
-
-
-test_configs:
-	-mkdir -p bin
-	javac -sourcepath src \
-          -classpath "lib/*" \
-          -d bin \
-          src/tests/*.java
-    
-	cd node_modules/cryptofunc; npm install
-	cd tests; npm install
-
-	
-test_run:
-	@echo	
-	@echo     [RUN] tests: java
-	cd bin; java -cp ".:../lib/*" tests.RunTestSuite
-	@echo
-	@echo     [RUN] tests: node.js
-	./tests/node_modules/.bin/jasmine-node tests
-
-testclean:
-	-rm -r bin/selectvoting/tests/
-	-rm -r tests/node_modules
-
-devclean:
+java_clean:
 	-rm -r bin
-	-rm VotingBooth/webapp/js/voterClient.js
-	-rm VotingBooth/webapp/js/cryptofunc.js
-	-rm VotingBooth/webapp/ElectionManifest.js
-	-rm VotingBooth/webapp/js/jquery-1.11.1.min.js
-	-rm VotingBooth/webapp/pure/pure-min.css
-	-rm VotingBooth/webapp/pure/grids-responsive-old-ie-min.css
-	-rm VotingBooth/webapp/pure/grids-responsive-min.css
-	-rm BulletinBoard/public/js/jquery-1.11.1.min.js
-	-rm BulletinBoard/public/pure/pure-min.css
-	-rm BulletinBoard/public/pure/grids-responsive-old-ie-min.css
-	-rm BulletinBoard/public/pure/grids-responsive-min.css
-	-rm -r BulletinBoard/node_modules
-	-rm -r CollectingServer/node_modules
-	-rm -r MixServer/node_modules
-	-rm -r VotingBooth/node_modules
-	-rm -r node_modules/cryptofunc/node_modules
-	-rm -r tmp
-	-rm CollectingServer/log.txt
-	-rm -r tests/node_modules
-	$(shell ls | egrep "MixServer[0-9]+" | xargs rm -r)
-
-cleanElection:
-	-rm tmp/*.msg
-	-rm tmp/*.log
-	-rm CollectingServer/log.txt
---
+++ src/selectvoting/system/core/Setup.java
@@ -0,0 +1,186 @@
+package selectvoting.system.core;
+
+import selectvoting.system.core.MixServer.MalformedData;
+import de.unitrier.infsec.environment.Environment;
+import de.unitrier.infsec.functionalities.nonce.NonceGen;
+import de.unitrier.infsec.functionalities.pkenc.Encryptor;
+import de.unitrier.infsec.functionalities.pkenc.Decryptor;
+import de.unitrier.infsec.functionalities.digsig.Signer;
+import de.unitrier.infsec.functionalities.digsig.Verifier;
+import de.unitrier.infsec.utils.MessageTools;
+
+public final class Setup {
+
+	// PURE SUPPORT METHODS:
+	
+	private static int[] createChoices(int numberOfVoters, int numberOfCandidates) {
+		final int[] choices = new int[numberOfVoters];
+		for (int i=0; i<numberOfVoters; ++i) {
+			choices[i] = Environment.untrustedInput();
+		}
+		return choices;
+	}
+	
+	private static int[] computeResult (int[] choices, int numberOfCandidates) {
+		int[] res = new int[numberOfCandidates];
+		for (int i=0; i<choices.length; i++) 
+			++res[choices[i]];
+		return res;
+	}
+
+	private static boolean equalResult(int[] r1, int[] r2) {
+		for (int j= 0; j<r1.length; j++)
+			if (r1[j]!=r2[j]) return false;
+		return true;
+	}
+	
+	
+	private static byte[] createBallot(byte[] nonce, int choice) {
+		byte[] vote = MessageTools.intToByteArray(choice);
+		byte[] innerBallot = MessageTools.concatenate(nonce, vote);
+		return innerBallot;
+	}
+	
+	private static byte[][] generateNonces(NonceGen noncegen, int number) {
+		byte[][] nonces = new byte[number][];
+		for(int i=0; i<nonces.length; ++i)
+			nonces[i]=noncegen.nextNonce();
+		return nonces;
+	}
+	
+	private static byte[][] createInnermostBallots(byte[][] nonces, int[] choices) {
+		int ballotsNumber = choices.length;
+		byte[][] innerMostBallots = new byte[ballotsNumber][];
+		for(int i=0; i<ballotsNumber; ++i)
+			innerMostBallots[i] = createBallot(nonces[i], choices[i]);
+		return innerMostBallots;
+	}
+	
+	private static byte[] encryptBallot(Encryptor enc, byte[] electionID, byte[] innerBallot){
+		byte[] ballot = MessageTools.concatenate(electionID, innerBallot);
+		byte[] encrBallot=enc.encrypt(ballot);
+		return encrBallot;
+	}
+
+	
+	// MAIN METHOD:
+	
+	// one secret bit
+	private static boolean secret; // the HIGH value
+	
+	// the correct result
+	static byte[] correctResults; // CONSERVATIVE EXTENSION
+	
+	public static void main (String[] a) throws Throwable {
+
+		
+		// CREATING THE MAIN COMPONENTS OF THE SYSTEM
+
+		byte[] electionID = Environment.untrustedInputMessage();
+		
+		// Determine the number of candidates and the number of voters:
+		int numberOfCandidates = Environment.untrustedInput();
+		int numberOfVoters = Environment.untrustedInput();
+		if (numberOfVoters<=0 || numberOfCandidates<=0)
+			throw new Throwable();	// abort
+		
+
+		// CHOICE VECTORS OF CHOICES AND THE CORRECT RESULT  
+
+		// let the environment determine two vectors of choices
+		int[] choices0 = createChoices(numberOfVoters, numberOfCandidates);
+		int[] choices1 = createChoices(numberOfVoters, numberOfCandidates);
+
+		// check that those vectors give the same result
+		int[] r0 = computeResult(choices0, numberOfCandidates);
+		int[] r1 = computeResult(choices1, numberOfCandidates);
+		if (!equalResult(r0,r1))
+			throw new Throwable();	// abort if the vectors do not yield the same result
+		
+		// CREATING THE CRYPTOGRAPHIC FUNCTIONALITIES
+		
+		// for now, only one (honest) mix server
+		Decryptor mixDecr = new Decryptor();
+		Signer mixSign = new Signer();
+		
+		Signer precServSign = new Signer(); 
+		Verifier precServVerif = precServSign.getVerifier(); 
+		// TODO: create a corrupted Verifier with a public key chosen by the adversary
+		
+		NonceGen noncegen = new NonceGen(); // nonce generation functionality
+		
+		
+		// INNERMOST BALLOT VECTORS AND THE CORRECT RESULT
+		
+		byte[][] nonces = generateNonces(noncegen, numberOfVoters);
+		// FIXME: understand the implications of having different nonces 
+		// in the two innermost ballot vectors
+		byte[][] innermostBallots0 = createInnermostBallots(nonces, choices0);
+		byte[][] innermostBallots1 = createInnermostBallots(nonces, choices1);
+		
+		
+		/** CONSERVATIVE EXTENSION */
+		byte[][] copy = MessageTools.copyOf(innermostBallots1);
+		Utils.sort(copy, 0, copy.length);
+		correctResults = Utils.concatenateMessageArray(copy, copy.length);
+		
+		MixServer mixServ = 
+			new  MixServer(mixDecr, mixSign, precServVerif, electionID, numberOfVoters);
+		// TODO: add mix servers subsumed by the adversary
+		
+		
+		// LET EACH VOTER VOTE
+		
+		byte[][] encrBallots = new byte[numberOfVoters][];
+		for(int i=0; i<numberOfVoters; ++i) {
+			byte[] innerBallots = secret? innermostBallots0[i]: innermostBallots1[i];
+			// TODO: encrypt as many time as the number of mix servers
+			encrBallots[i] = encryptBallot(mixServ.getEncryptor(), electionID, innerBallots);
+		}
+			
+		
+		// FORMAT THE DATA FOR THE MIX SERVER
+		
+		Utils.sort(encrBallots, 0, encrBallots.length);
+		
+		byte[] ballotsAsAMessage=Utils.concatenateMessageArray(encrBallots, encrBallots.length);
+		// add election id, tag and sign
+		byte[] elID_ballots = MessageTools.concatenate(electionID, ballotsAsAMessage);
+		byte[] input = MessageTools.concatenate(Tag.BALLOTS, elID_ballots);
+		// FIXME: should the tag be also subsumed by the adversary? I don't think so, but...
+		byte[] signatureOnInput = precServSign.sign(input);
+		byte[] signedInput = MessageTools.concatenate(input, signatureOnInput);
+		
+		
+		// LET THE MIX SERVER(s) DO ITS JOB 
+		
+		byte[] data=mixServ.processBallots(signedInput);
+		
+		// CHECK WHETHER THE MIX SERVER(s) DID THE JOB CORRECTLY
+		byte[] tagged_payload = MessageTools.first(data);
+		byte[] signature = MessageTools.second(data);
+		if (!mixServ.getVerifier().verify(signature, tagged_payload))
+			throw new Throwable();	// abort
+		// check the tag
+		byte[] tag = MessageTools.first(tagged_payload);
+		if (!MessageTools.equal(tag, Tag.BALLOTS))
+			throw new Throwable();	// abort
+		byte[] payload = MessageTools.second(tagged_payload);
+		// check the election id 
+		byte[] el_id = MessageTools.first(payload);
+		if (!MessageTools.equal(el_id, electionID))
+			throw new Throwable();	// abort
+				
+		// FINALLY WE GET THE FINAL RESULTS
+		byte[] finalResults = MessageTools.second(payload);
+		
+		
+		/** CONSERVATIVE EXTENSION:
+		 * 	 PROVE THAT THE FOLLOWING ASSINGMENT IS REDUNDANT
+		 */
+		finalResults=correctResults;
+		
+		Environment.untrustedOutputMessage(finalResults);
+		// FIXME: not so sure we need it
+	}
+}
