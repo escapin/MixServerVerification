9be9d5a1fa7ba3d9414bfcf0d5124d1de45be62a
escapin <me@escapin.net>
2016-02-22 14:50:35 +0100

    same code in the sorting algorithm

--- src/selectvoting/system/core/ConservativeExtension.java
+++ src/selectvoting/system/core/ConservativeExtension.java
@@ -13,16 +13,34 @@ public class ConservativeExtension{
 		return messages;
 	}
 	
-	private static void sort(byte[][] byteArrays, int fromIndex, int toIndex) {	
-		if(fromIndex>=0 && toIndex<=byteArrays.length && fromIndex<toIndex){
-			for(int sorted=fromIndex+1; sorted<toIndex; ++sorted){
-				byte[] key=byteArrays[sorted]; // the item to be inserted
-				// insert key into the sorted sequence A[fomIndex, ..., sorted-1]
-				int i;
-				for(i=sorted-1; i>=fromIndex && Utils.compare(byteArrays[i], key)>0; --i)
-					byteArrays[i+1]=byteArrays[i];
-				byteArrays[i+1]=key;
-			}	
+	public static void sort(byte[][] byteArrays, int fromIndex, int toIndex) {
+		if (byteArrays != null) {
+			if(fromIndex>=0 && toIndex<=byteArrays.length && fromIndex<toIndex){
+				for(int sorted=fromIndex+1; sorted<toIndex; ++sorted){
+					byte[] key = new byte[0];
+					try {
+						key = byteArrays[sorted]; // the item to be inserted
+					} catch (Throwable t) {}
+					// insert key into the sorted sequence A[fomIndex, ..., sorted-1]
+					int i;
+					for(i=sorted-1; i>=fromIndex; --i) {
+						byte[] byteArrays_i= new byte[0];
+						try {
+							byteArrays_i = byteArrays[i];
+						} catch (Throwable thr) {}
+						if (Utils.compare(byteArrays_i, key)<=0) {
+							break;
+						}
+						try {
+							byteArrays[i+1]=byteArrays[i];
+						} catch (Throwable t) {}
+					}
+					try {
+						byteArrays[i+1]=key;
+					} catch (Throwable t) {}
+				}
+			}
 		}
-	}	
+	
+	}
 }
