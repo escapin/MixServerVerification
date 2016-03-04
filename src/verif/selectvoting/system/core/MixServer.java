a7f5b31b5b0b25d8d738670f2eb1bd4cfcfc1a2e
Mihai Herda <herda@kit.edu>
2016-03-04 17:58:26 +0100

    Respecified the property.

--- src/verif/selectvoting/system/core/MixServer.java
+++ src/verif/selectvoting/system/core/MixServer.java
@@ -15,12 +15,14 @@ public class MixServer
 	private final Verifier precServVerif;
 	private final byte[] electionID;
 	// private final int numberOfVoters;
-	
+
+	 
+
 	// PUBLIC CLASSES
 	/**
 	 * Error thrown if the input data is ill-formed.
 	 */
-	
+
 	public static class MalformedData extends Exception {
 		public int errCode;
 		public String description;
@@ -32,7 +34,7 @@ public class MixServer
 			return "Mix Server Error: " + description;
 		}
 	}
-	
+
 	public static class ServerMisbehavior extends Exception {
 		public int errCode;
 		public String description;
@@ -44,9 +46,9 @@ public class MixServer
 			return "Previous Server Misbehavior: " + description;
 		}
 	}
-	
+
 	// CONSTRUCTORS
-	
+
 	public MixServer(Decryptor decryptor, Signer signer, Verifier precServVerif, byte[] electionID) {
 		this.signer = signer;
 		this.decryptor = decryptor;
@@ -54,8 +56,18 @@ public class MixServer
 		this.precServVerif = precServVerif;
 	}
 	
-	// PUBLIC METHODS
 	
+
+	// this is the randomly chosen message array
+	//@ public ghost instance byte[][] msg;
+	// this is the value of entr_arr after the call to Utils.sort
+	//@ public ghost instance byte[][] a;
+	// this is the value of entr_arr after the call to ConservativeExtenson.getRandomMessages
+	//@ public ghost instance byte[][] b;
+
+
+	// PUBLIC METHODS
+
 	/**
 	 * Process data that supposed to be the signed output of the preceding mix server. 
 	 * Returns the signed result of the mixing. 
@@ -66,12 +78,25 @@ public class MixServer
 	 * 			ENC_curr[elID, innerBallot] 
 	 * 
 	 */
+	/*
+	 * Assuming that the messages stored in the CE are a permutation of msg we prove
+	 * That the values in entr_arr after sort() are equal to the values of entr_arr after 
+	 * ConservativeExtension.retrieveSortetMessages().
+	 */
+	/*@
+	  requires \dl_seqPerm(\dl_array2seq(msg), \dl_array2seq(ConservativeExtension.messages)); 	  
+	  ensures a.length == b.length && (\forall int i; 0 <= i && i < a.length; a[i] == b[i]);
+	 @*/	
 	public byte[] processBallots(byte[] data) throws MalformedData, ServerMisbehavior {
+
 		
+
+
 		byte[] ballotsAsAMessage = checkAndGetBallots(data);
-		
+
 		byte[][] entr_arr = extractBallots(ballotsAsAMessage);
-		
+
+
 		/**
 		 * Assumption: the messages in the array variable 'entr_arr' above 
 		 * are a permutation of the messages in the array variable 'msg' in Setup.java
@@ -83,46 +108,53 @@ public class MixServer
 		 * 	 encrypted' 
 		 * is too demanding and time consuming for KeY.
 		 */
-		
+
+
 		entr_arr = sort(entr_arr); 
+		//@set a = entr_arr;
 		/** CONSERVATIVE EXTENSION:
 		 *   PROVE THAT THE FOLLOWING ASSINGMENT IS REDUNDANT
 		 */
 		entr_arr = ConservativeExtension.retrieveSortedMessages();
+		//@set b = entr_arr;
+
 		byte[] signedResult = postProcess(entr_arr);
-		
+
 		return signedResult;
 	}
-	
-	
-	
+
+
+
 
 	/**
 	 * TO BE PROVEN: The method returns the sorted input
 	 * By proving that Utils.sort also return the sorted input, we obtain that
 	 * the next assignment is redundant
 	 */
-	// this is the randomly chosen message array
-	//@ public ghost instance byte[][] msg;
-	
+
+
 	/*@
-	  requires entr_arr != null;
-	  requires \dl_seqPerm(\dl_array2seq(msg), \dl_array2seq(entr_arr));	  
-	  requires \dl_seqPerm(\dl_array2seq(msg), \dl_array2seq(ConservativeExtension.messages));
+	  requires entr_arr != null;	  
 	  ensures \dl_seqPerm(\dl_array2seq(\result), \dl_array2seq(entr_arr));
 	  ensures (\forall int i; 0 <= i && i < entr_arr.length; Utils.compare(entr_arr[i],entr_arr[i+1]) <= 0);	  
 	@*/	
 	private byte[][] sort(byte[][] entr_arr) {
-	    if (entr_arr != null) {
-		// sort the entries
-		Utils.sort(entr_arr, 0, entr_arr.length);
-		return entr_arr;
-	    } else {
-	        return new byte[][] {};
-	    }
+		if (entr_arr != null) {
+			// sort the entries
+			Utils.sort(entr_arr, 0, entr_arr.length);
+			return entr_arr;
+		} else {
+			return new byte[][] {};
+		}
 	}
-	
-	
+	/*
+	 * We assume this method returns a permutation of the array 'msg'.
+	 */
+	/*@
+	  ensures \result != null;
+	  ensures \dl_seqPerm( \dl_array2seq(\result), \dl_array2seq(msg)); 
+	  assignable \strictly_nothing; 
+	 @*/
 	private byte[][] extractBallots(byte[] ballotsAsAMessage) throws ServerMisbehavior{
 		//ArrayList<byte[]> entries = new ArrayList<byte[]>();
 		EntryList entries = new EntryList();
@@ -152,17 +184,24 @@ public class MixServer
 			}
 			//++numberOfEntries;
 		}
-		
+
 		byte[][] entr_arr = new byte[entries.size()][];
 		entries.toArray(entr_arr);
 		return entr_arr;
 	}
-
+	/*
+	 * Since this method is called after the redundant statement, we don't care what it does.
+	 * We assume it doesn't change any fields.
+	 */
+	/*@
+	  ensures true;
+	  assignable \strictly_nothing;
+	 @*/
 	private byte[] postProcess(byte[][] entr_arr) {
 		// format entries as one message
 		byte[] entriesAsAMessage = Utils.concatenateMessageArray(entr_arr, entr_arr.length);
-		
-		
+
+
 		// add election id, tag and sign
 		byte[] elID_entriesAsAMessage = MessageTools.concatenate(electionID, entriesAsAMessage);
 		byte[] result = MessageTools.concatenate(Tag.BALLOTS, elID_entriesAsAMessage);
@@ -170,32 +209,39 @@ public class MixServer
 		byte[] signedResult = MessageTools.concatenate(result, signatureOnResult);
 		return signedResult;
 	}
-
-
+	
+	/*
+	 * We assume it doesn't change any fields.
+	 */
+	/*@
+	  public normal_behaviour
+	  ensures true;
+	  assignable \strictly_nothing;
+	 @*/
 	private byte[] checkAndGetBallots(byte[] data) throws MalformedData {
 		// verify the signature of previous server
 		byte[] tagged_payload = MessageTools.first(data);
 		byte[] signature = MessageTools.second(data);
 		if (!precServVerif.verify(signature, tagged_payload))
 			throw new MalformedData(1, "Wrong signature");
-		
+
 		// check the tag
 		byte[] tag = MessageTools.first(tagged_payload);
 		if (!MessageTools.equal(tag, Tag.BALLOTS))
 			throw new MalformedData(2, "Wrong tag");		
 		byte[] payload = MessageTools.second(tagged_payload);
-		
+
 		// check the election id 
 		byte[] el_id = MessageTools.first(payload);
 		if (!MessageTools.equal(el_id, electionID))
 			throw new MalformedData(3, "Wrong election ID");
-		
+
 		// retrieve and process ballots (store decrypted entries in 'entries')
 		byte[] ballotsAsAMessage = MessageTools.second(payload);
 		return ballotsAsAMessage;
 	}
-	
-	
+
+
 	// methods for testing
 	public Encryptor getEncryptor(){
 		return decryptor.getEncryptor();
